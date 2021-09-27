21d5e16a2ec92ab414cb6f181c3af59124f1549e
Vlad <60785395+vladmomot@users.noreply.github.com>
2021-09-27 15:26:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-
-
 const nav = document.querySelector(".nav-menu");
-const navBtnContainer = document.querySelector(".nav-btn-container");
-const searchBtn = document.querySelector(".search-btn");
+const ill = document.querySelector(".illustration1");
+const valpr = document.querySelector(".value-proposition");
+const head = document.querySelector("header");
+const btns = document.querySelector(".nav-btn-container");
+const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 
-searchBtn.addEventListener("click", () => {
-  
-  
+menuBtn.addEventListener("click", () => {
   nav.classList.add("active");
-  navBtnContainer.classList.add("active");
+  ill.classList.add("active");
+  valpr.classList.add("active");
+  head.classList.add("active");
+  btns.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
- 
   nav.classList.remove("active");
-  navBtnContainer.classList.remove("active");
+  ill.classList.remove("active");
+  valpr.classList.remove("active");
+  head.classList.remove("active");
+  btns.classList.remove("active");
 });
